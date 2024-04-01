ca7b7a1cfbc44956d6c07608b4c7caf3e27476e9
Anideep Kalia <anideepkalia@gmail.com>
2024-04-01 19:36:58 +0530

    init

---
+++ fundme/hardhat-fund-me-fcc/contracts/test/MockV3Aggregator.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+// so the MockV3Aggregator is used for testing the contract
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
